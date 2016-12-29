9701d3a997f7bb5446582d23d2acf33f25cffba2
Zaporozhec7 <Zaporozhec7@users.noreply.github.com>
2016-12-30 01:43:30 +0200

    - Change method to add script: Inject it now to page directly instead using as chrome extension content script
    - Almost complete image uploading process

--- js/FatTweet.class.js
+++ js/FatTweet.class.js
@@ -1,11 +1,13 @@
 class FatTweet {
-    constructor(){
+    constructor(extensionRoot){
+        this._extensionRoot = extensionRoot;
+        this.setCurrentData();
         this.processTweetBoxes();
         var FT = this;
         $('body').on('click', '.fat-tweet-convert-text', function(e){
             e.preventDefault();
             var $form = $(this).closest('form');
-            FT.convertText($form);
+            FT.convertTextToCanvas($form);
         });
     }
     processTweetBoxes(){
@@ -21,7 +23,7 @@ class FatTweet {
         });
     }
     processTweetForm($form){
-        var $button = $('<button class="btn fat-tweet-convert-text js-tooltip" data-delay="150" data-original-title="Convert text into image" type="button"><img src="'+chrome.runtime.getURL('img/32.png')+'"></button>');
+        var $button = $('<button class="btn fat-tweet-convert-text js-tooltip" data-delay="150" data-original-title="Convert text into image" type="button"><img src="'+this._extensionRoot+'img/32.png"></button>');
         $form
             .addClass('fat-tweet-processed-form')
             .find('.btn.tweet-btn')
@@ -31,19 +33,103 @@ class FatTweet {
     isTweetFormProcessed($form){
         return $form.hasClass('fat-tweet-processed-form');
     }
-    convertText($form){
+    convertTextToCanvas($form){
         var FT = this;
+        FT.setCurrentData();
         var $area = $form.find('.tweet-box[name="tweet"]');
         $area.addClass('screenshot-process');
         html2canvas($area.get(0), {
             onrendered: function(canvas) {
+                FT.setCurrentData($form, FT.getBlobFromCanvas(canvas), 'image/jpeg');
                 $area.after(canvas);
                 $area
                     .removeClass('screenshot-process')
                     .html(FT.getLinksClean($area.children('div')));
                 $area.focus();
+                FT.initUpload();
             }
         });
+    }
+    setCurrentData($form = null, imageBlob = null, imageType = null){
+        this._$currentForm = $form;
+        this._currentImageBlob = imageBlob;
+        this._currentImageType = imageType;
+    }
+    getBlobFromCanvas(canvas, type = 'image/jpeg', quality = 0.75){
+        var dataURL = canvas.toDataURL(type, quality);
+        return this.dataURItoBlob(dataURL);
+    }
+    initUpload(){
+        var FT = this;
+        var url = 'https://upload.twitter.com/i/media/upload.json?command=INIT&total_bytes='
+        + this._currentImageBlob.size
+        + '&media_type='
+        + this._currentImageType
+        + '&media_category=tweet_image';
+        // Can't use $.ajax here, because
+        // Twitter return HTTP status 202 here
+        // and jQuery.ajax do not fire "success"
+        // or .done() for that response
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open('POST', url, true);
+        xmlHttp.withCredentials = true;
+        xmlHttp.onreadystatechange = function() {
+            if (xmlHttp.readyState === 4) {
+                if (xmlHttp.status === 202) {
+                    var data = JSON.parse(xmlHttp.responseText);
+                    FT.appendUpload(data.media_id_string);
+                } else {
+                    console.error('Fat Tweet: Something went wrong on media INIT stage');
+                }
+            }
+        };
+        xmlHttp.send();
+    }
+    appendUpload(mediaId){
+        var FT = this;
+        var url = 'https://upload.twitter.com/i/media/upload.json?command=APPEND&media_id='
+        + mediaId
+        + '&segment_index=0';
+        var fd = new FormData();
+        fd.append('media', this._currentImageBlob, 'blob');
+        $.ajax(
+            url,
+            {
+                type: 'POST',
+                data: fd,
+                contentType: false,
+                processData: false,
+                xhrFields: {
+                    withCredentials: true,
+                }
+            })
+            .done(function(){
+                FT.finalizeUpload(mediaId);
+            })
+            .fail(function(){
+                console.error('Fat Tweet: Something went wrong on media APPEND stage');
+            });
+    }
+    finalizeUpload(mediaId){
+        var FT = this;
+        var url = 'https://upload.twitter.com/i/media/upload.json?command=FINALIZE&media_id='
+        + mediaId;
+        var fd = new FormData();
+        fd.append('media', this._currentImageBlob, 'blob');
+        $.ajax(
+            url,
+            {
+                type: 'POST',
+                xhrFields: {
+                    withCredentials: true,
+                }
+            })
+            .done(function(){
+
+            })
+            .fail(function(){
+                console.error('Fat Tweet: Something went wrong on media FINALIZE stage');
+            });
     }
     getLinksClean($content){
         var $links = $content.find('a');
@@ -58,5 +144,39 @@ class FatTweet {
             $newContent.append($(this)).append(' ');
         });
         return $newContent;
+    }
+    appendAlert(text, type = 'status'){
+
+    }
+    /**
+    * This great function was taken here:
+    * https://stackoverflow.com/questions/4998908/convert-data-uri-to-file-then-append-to-formdata/5100158#5100158
+    */
+    dataURItoBlob(dataURI) {
+        // convert base64/URLEncoded data component to raw binary data held in a string
+        var byteString;
+        if (dataURI.split(',')[0].indexOf('base64') >= 0)
+            byteString = atob(dataURI.split(',')[1]);
+        else
+            byteString = unescape(dataURI.split(',')[1]);
+
+        // separate out the mime component
+        var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+        // write the bytes of the string to a typed array
+        var ia = new Uint8Array(byteString.length);
+        for (var i = 0; i < byteString.length; i++) {
+            ia[i] = byteString.charCodeAt(i);
+        }
+
+        return new Blob([ia], {type:mimeString});
+    }
+    /**
+     * For now this is just placeholder for translate function
+     * @param  string text String that should be translated
+     * @returns string Translated string
+     */
+    t(string){
+        return string;
     }
 }
--- js/init.js
+++ js/init.js
@@ -1,18 +1,20 @@
+// WARNUNG! Code below look little awful
+// but we need all that hacks
+// We should run script in page context
+// because when upload image we need
+// that data sent with needed cookies and referrer.
+// Pull requests with cleaner solution are welcome
+var _html2canvas_src = chrome.runtime.getURL('bower_components/html2canvas/build/html2canvas.min.js');
+var _jquery_observe_src = chrome.runtime.getURL('bower_components/jquery-observe/jquery-observe.js');
+var _strip_tags_src = chrome.runtime.getURL('js/strip_tags.js');
+var _FatTweet_src = chrome.runtime.getURL('js/FatTweet.class.js');
+var _extensionRoot = chrome.runtime.getURL('/');
+var _initScript = '<script>$(document).ready(function(){$.getScript("'+_html2canvas_src+'",function(){$.getScript("'+_jquery_observe_src+'", function(){$.getScript("'+_strip_tags_src+'", function(){$.getScript("'+_FatTweet_src+'", function(){var FT = new FatTweet("'+_extensionRoot+'");$("body").observe("childlist", ".tweet-box", function(record) {FT.processTweetBoxes();});});});});});});</script>';
+
 $(document).ready(function(){
-    // Some styles that we need
-    $('head').append('<link rel="stylesheet" type="text/css" href="'+chrome.runtime.getURL('css/content.css')+'">');
-    // File js/FatTweet.class.js
-    var FT = new FatTweet();
-    /**
-    * We should process new tweet forms when them added.
-    * But Twitter very actively use Ajax and other DOM
-    * manipulations that can be performed without Ajax.
-    * So let's use Mutations observer for this,
-    * because this is Chrome and we can:)
-    * Call FatTweet.processTweetBoxes() when
-    * new boxes added.
-    */
-    $('body').observe('childlist', '.tweet-box[name="tweet"]', function(record) {
-        FT.processTweetBoxes();
-    });
+    // Append some styles that we need
+    // and init script
+    $('head')
+        .append('<link rel="stylesheet" type="text/css" href="'+chrome.runtime.getURL('css/content.css')+'">')
+        .append(_initScript);
 });
